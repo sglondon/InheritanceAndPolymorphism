92e258e503a47a3bfab46570052a6d2b6c4408e2
sglondon <susangs2@gmail.com>
2018-02-05 11:48:40 -0500

    first commit

---
+++ Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndPoloymorphismPractice
+{
+    public class Rectangle : Shape
+    {
+        private double area;
+
+        public double Area
+        {
+            get { return this.area; }
+            set { this.area = value; }
+        }
+
+        public Rectangle(double area)
+        {
+            this.area = area;
+        }
+    }
+
+
+    
+    }
+
---
+++ Shape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndPoloymorphismPractice
+{
+   public class Shape
+    {
+        //Fields
+        private int sides;
+
+        //Properties
+        public int Side
+        {
+            get { return this.sides; }
+            set { this.sides = value; }
+        }
+
+        //Constructors
+
+        public Shape()
+        {
+            //default constructor
+        }
+
+        public Shape(int sides)
+        {
+            this.sides = sides;
+        }
+
+        //Methods
+    }
+}
