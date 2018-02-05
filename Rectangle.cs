97e3230017b81d592da72ec55324860e0551fd1e
sglondon <susangs2@gmail.com>
2018-02-05 13:34:58 -0500

    adding classes

--- Rectangle.cs
+++ Rectangle.cs
@@ -6,15 +6,22 @@ namespace InheritanceAndPoloymorphismPractice
 {
     public class Rectangle : Shape
     {
+        //field
         private double area;
+
+        //properties         has property that shape class has as well
 
         public double Area
         {
             get { return this.area; }
             set { this.area = value; }
         }
+        //derived class does not inherit the constructors
+        //of the base class; however constructors of
+        //the base class can be accessed using
+        //the "base" keyword
 
-        public Rectangle(double area)
+        public Rectangle(double area) //tells to use sides 4 from  base class constructor : base(4)
         {
             this.area = area;
         }
